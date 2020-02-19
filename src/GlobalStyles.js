fddd59a7f951d366f314492ec9fb6d1236a8ee33
Amanat <amnaatmurzaliev@gmail.com>
2020-02-19 12:36:56 -0800

    create-react-app

---
+++ src/GlobalStyles.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -webkit-tap-highlight-color: rgba(0,0,0,0);
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  }
+  input {
+    font-size: inherit;
+  }
+  img {
+    max-width: 100%;
+  }
+  button {
+    border: none;
+    cursor: pointer;
+    outline: none
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+
+export default GlobalStyles
